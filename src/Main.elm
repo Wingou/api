4f23b464349ce313ef69581a9a3940ceca3c12bb
Wingou <wing92300@gmail.com>
2019-12-19 20:47:13 +0100

    afficher Record

--- src/Main.elm
+++ src/Main.elm
@@ -5,20 +5,35 @@ import Html exposing (Html, a, button, div, img, text)
 import Html.Attributes exposing (src, style)
 import Html.Events exposing (onClick)
 import Http
-import Json.Decode exposing (Decoder, field, string)
+import Json.Decode exposing (Decoder, at, field, int, map3, string)
+import String exposing (fromInt)
 
 
 
 -- MODEL
 
 
+type alias Meta =
+    { status : Int
+    , msg : String
+    , response_id : String
+    }
+
+
 type alias Model =
-    { result : String }
+    { result : Meta }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { result = "RIEN" }, Cmd.none )
+    ( { result =
+            { status = 0
+            , msg = "RIEN"
+            , response_id = "RIENNULL"
+            }
+      }
+    , Cmd.none
+    )
 
 
 
@@ -27,24 +42,27 @@ init =
 
 type Msg
     = NoOp
-    | GotText (Result Http.Error String)
+    | GotText (Result Http.Error Meta)
     | CallGetApi
 
 
 getPublicOpinion : Cmd Msg
 getPublicOpinion =
     Http.get
-        { 
-            -- url = "https://elm-lang.org/assets/public-opinion.txt"
-              --url = "https://ci-api-mediashare.vpback.vpgrp.io/api/context?page=1&size=1"
-             url="https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
-            , expect = Http.expectJson GotText resultDecoder
+        { -- url = "https://elm-lang.org/assets/public-opinion.txt"
+          --url = "https://ci-api-mediashare.vpback.vpgrp.io/api/context?page=1&size=1"
+          url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
+        , expect = Http.expectJson GotText resultDecoder
         }
 
 
-resultDecoder : Decoder String
-resultDecoder = 
-    field "data" (field "image_url" string)
+resultDecoder : Decoder Meta
+resultDecoder =
+    map3 Meta
+        (at [ "meta", "status" ] int)
+        (at [ "meta", "msg" ] string)
+        (at [ "meta", "response_id" ] string)
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -56,11 +74,20 @@ update msg model =
             ( model, getPublicOpinion )
 
         GotText r ->
-            ( {model| result = 
+            ( { model
+                | result =
                     case r of
-                        Ok s -> s
-                        Err err -> "no"
-                    }  , Cmd.none )
+                        Ok s ->
+                            s
+
+                        Err err ->
+                            { status = 0
+                            , msg = "RIEN_ERROR"
+                            , response_id = "RIENNULL_ERROR"
+                            }
+              }
+            , Cmd.none
+            )
 
 
 
@@ -75,7 +102,9 @@ apiGet =
 view : Model -> Html Msg
 view model =
     div []
-        [ div [] [ text model.result ]
+        [ div [] [ text ("STATUS : " ++ fromInt model.result.status) ]
+        , div [] [ text ("MSG :  " ++ model.result.msg) ]
+        , div [] [ text ("RESPONSE_ID :  " ++ model.result.response_id) ]
         , img [ src "images/door.jpg" ] []
         , a [ style "cursor" "pointer", onClick CallGetApi ] [ text "Call API" ]
         ]
